466d44b6ceea78c749d21110f211fc18d4d18872
kingkasrak-beep <kingkasrak@gmail.com>
2025-12-28 15:59:46 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
-API_TOKEN = os.getenv("API_TOKEN")  # تو Render ست می‌کنی
+API_TOKEN = os.getenv("API_TOKEN")
+if not API_TOKEN:
+    raise RuntimeError("API_TOKEN is not set")
+
 OWNER_ID = 6321580395
